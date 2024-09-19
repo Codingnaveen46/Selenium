c57789c4872e3f297b95bfdfd7c851a887c35291
codingnaveen46 <naveenreddy4411@.com>
2024-09-19 21:41:45 +0530

    selenium learning

--- src/main/java/org/example/FirstSeleniumClass.java
+++ src/main/java/org/example/FirstSeleniumClass.java
@@ -3,7 +3,7 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
+public class FirstSeleniumClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
