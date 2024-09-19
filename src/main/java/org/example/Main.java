a5013fd754ad5d0fad1117d282b66a20132665db
codingnaveen46 <naveenreddy4411@.com>
2024-09-19 21:36:59 +0530

    selenium

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.quit();
+
+        /*
+            //selenium can automate web application
+    //cross browser testing
+    // multi-language support
+    // platform independent
+    //parallel executions
+    // can integrate with other tools
+    //open source
+
+    //challenges
+
+    //cant test desktop application
+    //directly cant automate mobile app
+    //performance testing
+    //no built-in reporting mechanism
+
+
+    //how selenium works
+    //3 components
+
+    //1 selenium client library
+    //2 browser driver
+    //3 browser
+
+    public static void main(String[] args) {
+
+        //launch browser
+       // System.setProperty("web driver.edge.driver", "/Path of the driver"
+      //  WebDriver driver = new ChromeDriver();// System.driver = new EdgeDriver();
+
+
+        //learnt about selenium firstclass
+         */
+
+
+
+        // first selenium class
+
+    }
+}
